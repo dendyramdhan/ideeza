362a4a1b95fff038b893e40bfe09ec671934c6c6
Jawad <jawainc@gmail.com>
2019-12-03 23:46:15 +0500

    user and technician updates

---
+++ components/user/pro/pro-electronics.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+
+
+
+
+
+  </div>
+</template>
+
+<script>
+
+    export default {
+        name: "pro-electronics",
+
+      data() {
+        return {
+          visibleGrid: true,        // show the grid
+          initDataForEngine: {},    // the entire object need to init the engine, at the begining is empty
+          initDataForPins: {
+            need: {},
+            pass: {}
+          },                        // the entire object need on adding pins
+          doAction: "move",         // move, rotate, copy, delete, label, route, highlight
+          resolution: 100,           // min: 1, max: 100.
+          partDataForImport: {}     // the entire object need to import a part
+        }
+      },
+      methods: {
+
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
