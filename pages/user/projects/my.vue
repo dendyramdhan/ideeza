ee883d29330acd083029bb53acdf39cbd8c8f8da
Alive Handsome <hwolf0610@hotmail.com>
2020-01-02 17:45:12 +0800

    User Project _first_page_

--- pages/user/projects/my.vue
+++ pages/user/projects/my.vue
@@ -12,101 +12,41 @@
           </div>
 
           <div class="lg:flex flex-wrap">
-            <div
-              class="w-32p project-item-container w-full shadow border border-solid border-light-gray mt-12 relative"
-               v-for="Project in Projects" :key="Project" v-if="Project.flag == 1"
-            >
-            
-                  <nuxt-link to="/user/projects/detail">
-                    <div class="image-container">
-                      <img class="project-item-container--image" src="~/static/images/car-big.png" alt />
-                    </div>
-                    <div class="flex justify-between items-center text-xs text-ideeza-black mt-5">
-                      <div class="flex items-center">
-                        <font-awesome-icon class="mr-2 h-4 text-ideeza-gold" :icon="['fas', 'star']" />
-                        <span>{{Project.mark}}</span>
-                      </div>
-                      <span class="font-semibold">{{Project.commitmember1}} &nbsp; {{Project.commitkind1}}&nbsp; {{Project.commitmember2}}&nbsp; {{Project.commitkind2}}</span>
-                      
-                    </div>
-                  </nuxt-link>
+                        <div
+                        class="w-32p project-item-container w-full shadow border border-solid border-light-gray mt-12 relative"
+                        >
+                                <nuxt-link to="/user/projects/detail">
+                                    <div class="image-container">
+                                    <img class="project-item-container--image" src="~/static/images/car-big.png" alt />
+                                    </div>
+                                    <div class="flex justify-between items-center text-xs text-ideeza-black mt-5">
+                                    <div class="flex items-center">
+                                        <font-awesome-icon class="mr-2 h-4 text-ideeza-gold" :icon="['fas', 'star']" />
+                                        <span>4.5</span>
+                                    </div>
+                                    <span class="font-semibold">24 likes</span>
+                                    </div>
+                                </nuxt-link>
 
-              <div
-                class="absolute p-5 rounded-lg border-light-gray border-solid border shadow-md z-50 pop-over"
-              >
-                <div class="flex justify-between items-center">
-                  <div class="flex items-center">
-                    <h1 class="font-semibold text-lg">{{Project.projectName}}</h1>
-                    <span class="text-sm text-gray-600 ml-3">{{Project.projecttitle}}</span>
-                  </div>
-                  <div class="font-semibold text-ideeza-black">{{Project.Cost}}</div>
-                </div>
+                                <div
+                                    class="absolute p-5 rounded-lg border-light-gray border-solid border shadow-md z-50 pop-over"
+                                >
+                                    <div class="flex justify-between items-center">
+                                    <div class="flex items-center">
+                                        <h1 class="font-semibold text-lg">Project name</h1>
+                                        <span class="text-sm text-gray-600 ml-3">Electronics + Cover</span>
+                                    </div>
+                                    <div class="font-semibold text-ideeza-black">$20,000</div>
+                                    </div>
 
-                <div class="text-sm mt-5">{{Project.ShortDescription}} </div>
-              </div>
-              </div>
-              
-              <div v-else class="w-64p double flex justify-between project-item-container w-full shadow border border-solid border-light-gray mt-12 relative">                
-               
-              <nuxt-link to="/user/projects/detail">
-                <div class="image-container">
-                  <img class="project-item-container--image" src="~/static/images/car-big.png" alt />
-                </div>
-                <div class="flex justify-between items-center text-xs text-ideeza-black mt-5">
-                  <div class="flex items-center">
-                    <font-awesome-icon class="mr-2 h-4 text-ideeza-gold" :icon="['fas', 'star']" />
-                    <span>{{Project.mark.first}}</span>
-                  </div>
-                  <span class="font-semibold">{{Project.commitmember1.first}} &nbsp; {{Project.commitkind1.first}}</span>
-                </div>
-              </nuxt-link>
-              <nuxt-link to="/user/projects/detail" class="ml-5">
-                <div class="image-container">
-                  <img class="project-item-container--image" src="~/static/images/car-big.png" alt />
-                </div>
-                <div class="flex justify-between items-center text-xs text-ideeza-black mt-5">
-                  <div class="flex items-center">
-                    <font-awesome-icon class="mr-2 h-4 text-ideeza-gold" :icon="['fas', 'star']" />
-                    <span>{{Project.mark.second}}</span>
-                  </div>
-                  <span class="font-semibold">{{Project.commitmember1.second}} &nbsp;{{Project.commitkind1.second}}</span>
-                </div>
-              </nuxt-link>
-
-              <div
-                class="absolute p-5 rounded-lg border-light-gray border-solid border shadow-md z-50 bg-white pop-over"
-              >
-                <div class="flex justify-between items-center">
-                  <div class="flex items-center">
-                    <h1 class="font-semibold text-lg">{{Project.projectName.first}}</h1>
-                    <span class="text-sm text-gray-600 ml-3">{{Project.projecttitle.first}}</span>
-                  </div>
-                  <div class="font-semibold text-ideeza-black">{{Project.Cost.first}}</div>
-                </div>
-
-                <div class="text-sm mt-5">
-                  {{Project.ShortDescription.first}}
-                </div>
-
-                <div class="flex justify-between items-center mt-10">
-                  <div class="flex items-center">
-                    <h1 class="font-semibold text-lg">{{Project.projectName.second}}</h1>
-                    <span class="text-sm text-gray-600 ml-3">{{Project.projecttitle.first}}</span>
-                  </div>
-                  <div class="font-semibold text-ideeza-black">{{Project.Cost.second}}</div>
-                </div>
-
-                <div class="text-sm mt-5">
-                  {{Project.ShortDescription.second}}
-                </div>
-              </div>
+                                    <div class="text-sm mt-5">
+                                    Sed in porta nunc, a interdum massa. Donec elementum id leo in pulvinar.
+                                    Cras purus erat, feugiat vel lectus ut, hendrerit porttitor sem. Curabitur
+                                    tempor sem ex
+                                    </div>
+                            </div>
             </div>
-             
-            
-
-
-            <!-- 
-              <div
+            <div
               class="w-64p double flex justify-between project-item-container w-full shadow border border-solid border-light-gray mt-12 relative"
             >
               <nuxt-link to="/user/projects/detail">
@@ -165,9 +105,110 @@
                   tempor sem ex
                 </div>
               </div>
-            </div> -->
+            </div>
 
+            <div
+              class="w-32p project-item-container w-full shadow border border-solid border-light-gray mt-12 relative"
+            >
+              <nuxt-link to="/user/projects/detail">
+                <div class="image-container">
+                  <img class="project-item-container--image" src="~/static/images/car-big.png" alt />
+                </div>
+                <div class="flex justify-between items-center text-xs text-ideeza-black mt-5">
+                  <div class="flex items-center">
+                    <font-awesome-icon class="mr-2 h-4 text-ideeza-gold" :icon="['fas', 'star']" />
+                    <span>4.5</span>
+                  </div>
+                  <span class="font-semibold">24 likes</span>
+                </div>
+              </nuxt-link>
 
+              <div
+                class="absolute p-5 rounded-lg border-light-gray border-solid border shadow-md z-50 bg-white pop-over"
+              >
+                <div class="flex justify-between items-center">
+                  <div class="flex items-center">
+                    <h1 class="font-semibold text-lg">Project name</h1>
+                    <span class="text-sm text-gray-600 ml-3">Electronics + Cover</span>
+                  </div>
+                  <div class="font-semibold text-ideeza-black">$20,000</div>
+                </div>
+
+                <div class="text-sm mt-5">
+                  Sed in porta nunc, a interdum massa. Donec elementum id leo in pulvinar.
+                  Cras purus erat, feugiat vel lectus ut, hendrerit porttitor sem. Curabitur
+                  tempor sem ex
+                </div>
+              </div>
+            </div>
+            <div
+              class="w-32p project-item-container w-full shadow border border-solid border-light-gray mt-12 relative"
+            >
+              <nuxt-link to="/user/projects/detail">
+                <div class="image-container">
+                  <img class="project-item-container--image" src="~/static/images/car-big.png" alt />
+                </div>
+                <div class="flex justify-between items-center text-xs text-ideeza-black mt-5">
+                  <div class="flex items-center">
+                    <font-awesome-icon class="mr-2 h-4 text-ideeza-gold" :icon="['fas', 'star']" />
+                    <span>4.5</span>
+                  </div>
+                  <span class="font-semibold">24 likes</span>
+                </div>
+              </nuxt-link>
+
+              <div
+                class="absolute p-5 rounded-lg border-light-gray border-solid border shadow-md z-50 bg-white pop-over"
+              >
+                <div class="flex justify-between items-center">
+                  <div class="flex items-center">
+                    <h1 class="font-semibold text-lg">Project name</h1>
+                    <span class="text-sm text-gray-600 ml-3">Electronics + Cover</span>
+                  </div>
+                  <div class="font-semibold text-ideeza-black">$20,000</div>
+                </div>
+
+                <div class="text-sm mt-5">
+                  Sed in porta nunc, a interdum massa. Donec elementum id leo in pulvinar.
+                  Cras purus erat, feugiat vel lectus ut, hendrerit porttitor sem. Curabitur
+                  tempor sem ex
+                </div>
+              </div>
+            </div>
+            <div
+              class="w-32p project-item-container w-full shadow border border-solid border-light-gray mt-12 relative"
+            >
+              <nuxt-link to="/user/projects/detail">
+                <div class="image-container">
+                  <img class="project-item-container--image" src="~/static/images/car-big.png" alt />
+                </div>
+                <div class="flex justify-between items-center text-xs text-ideeza-black mt-5">
+                  <div class="flex items-center">
+                    <font-awesome-icon class="mr-2 h-4 text-ideeza-gold" :icon="['fas', 'star']" />
+                    <span>4.5</span>
+                  </div>
+                  <span class="font-semibold">24 likes</span>
+                </div>
+              </nuxt-link>
+
+              <div
+                class="absolute p-5 rounded-lg border-light-gray border-solid border shadow-md z-50 bg-white pop-over"
+              >
+                <div class="flex justify-between items-center">
+                  <div class="flex items-center">
+                    <h1 class="font-semibold text-lg">Project name</h1>
+                    <span class="text-sm text-gray-600 ml-3">Electronics + Cover</span>
+                  </div>
+                  <div class="font-semibold text-ideeza-black">$20,000</div>
+                </div>
+
+                <div class="text-sm mt-5">
+                  Sed in porta nunc, a interdum massa. Donec elementum id leo in pulvinar.
+                  Cras purus erat, feugiat vel lectus ut, hendrerit porttitor sem. Curabitur
+                  tempor sem ex
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -177,15 +218,12 @@
 
 <script>
 import LeftMenu from "~/components/user/common-left-side-menu.vue";
-import Projects from "../../../data/UserProjectApi.json";
-
 export default {
   layout: "user",
   name: "projects-index",
   data: function() {
     return {
-      longview: true,
-      Projects:Projects
+      longview: true
     };
   },
   components: {
