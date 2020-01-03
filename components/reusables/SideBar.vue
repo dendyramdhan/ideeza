075de0143472ea17045ac9367b6356333b181183
DESKTOP-N7IC7EF\snowman <alivesoftsnowman@outlook.com>
2020-01-04 04:20:14 +0800

    update user auth

---
+++ components/reusables/SideBar.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="flex flex-wrap items-start bg-white h-full shadow relative side-bar">
+      <div class="p-5 relative w-full">
+            <div class="sider-bar left-main-menu ">
+                <template v-for="(item) in menuItems">
+                    <nuxt-link :to="item.link">
+                        <template v-if="item.iconComponent">
+                            <component :is="item.iconComponent" class="fill-current mr-5" />
+                        </template>
+                        <template v-else-if="item.icon">
+                            <font-awesome-icon class="text-gray-500 mr-5 text-2xl" :icon="['fa', item.icon]" />
+                        </template>
+                        {{item.name}}
+                    </nuxt-link>
+                    <template v-if="item.childs">
+                        <template v-for="(item) in item.childs">
+                            <nuxt-link :to="item.link" class="ml-10">
+                                <span class="ml-1">{{item.name}}</span>
+                            </nuxt-link>
+                        </template>
+                    </template>
+                </template>
+          </div> 
+      </div>
+  </div>
+</template>
+<script>
+  import DashBoardIcon from '~/components/partials/icons/dashboard-icon.vue'
+  import ProjectsIcon from '~/components/partials/icons/bars-icon.vue'
+  import TaskIcon from '~/components/partials/icons/list-icon.vue'
+    export default {
+        props: {
+            menuItems: {
+                type: Array,
+                default: () => {
+                    return []
+                }
+            }
+        },
+        components: {
+            DashBoardIcon,
+            ProjectsIcon,
+            TaskIcon
+        }
+    }
+</script>
+<style>
+.side-bar{
+    width: 270px;
+    min-width: 270px;
+  }
+  .left-main-menu{
+
+  }
+  .menu-item{
+    @apply flex mb-8 content-center items-center mt-2 text-gray-700 text-xl font-semibold cursor-pointer;
+  }
+
+  .menu-item:hover{
+    @apply text-ideeza;
+  }
+  .left-main-menu svg{
+    @apply text-gray-500;
+  }
+  .menu-item:hover svg{
+    @apply text-ideeza;
+  }
+  .active-link,
+  .active-link svg{
+    @apply text-ideeza;
+  }
+  .sider-bar a:visited,.sider-bar a{
+    @apply block p-5;
+  }
+</style>
---
+++ pages/service-providers/projects/index.vue
@@ -0,0 +1,237 @@
+<template>
+  <div class="main-contents">
+    <div class="md:flex items-center justify-between">
+      <h1 class="text-2xl font-bold mr-5">Good Morning Chan!</h1>
+      <div class="md:flex justify-end">
+        <div class="bg-white rounded border shadow md:mr-3 mb-3 md:mb-0 py-3 px-5">
+            <div class="flex items-center">
+            <div>
+                <div class="text-sm">Reviews</div>
+                <div class="text-3xl font-bold">56</div>
+            </div>
+            <div>
+                <font-awesome-icon class="text-4xl mt-2 ml-4 text-ideeza" :icon="['fas', 'envelope']" />
+            </div>
+            </div>
+        </div>
+        <div class="bg-white rounded border shadow md:mr-3 mb-3 md:mb-0 py-3 px-5">
+            <div class="flex items-center">
+            <div>
+                <div class="text-sm">Money earned</div>
+                <div class="text-3xl font-bold">56</div>
+            </div>
+            <div>
+                <font-awesome-icon class="text-4xl mt-2 ml-4 text-ideeza" :icon="['fas', 'envelope']" />
+            </div>
+            </div>
+        </div>
+        <div class="bg-white rounded border shadow md:mr-3 mb-3 md:mb-0 py-3 px-5">
+            <div class="flex items-center">
+            <div>
+                <div class="text-sm">Opened orders</div>
+                <div class="text-3xl font-bold">56</div>
+            </div>
+            <div>
+                <font-awesome-icon class="text-4xl mt-2 ml-4 text-ideeza" :icon="['fas', 'envelope']" />
+            </div>
+            </div>
+        </div>
+        <div class="bg-white rounded border shadow md:mr-3 mb-3 md:mb-0 py-3 px-5">
+            <div class="flex items-center">
+            <div>
+                <div class="text-sm">Stars</div>
+                <div class="text-3xl font-bold">56</div>
+            </div>
+            <div>
+                <font-awesome-icon class="text-4xl mt-2 ml-4 text-ideeza" :icon="['fas', 'envelope']" />
+            </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="text-sm font-bold text-black md:ml-5 mb-3">My Feed</div>
+    <div class="md:flex">
+      <div class="md:w-9/12 mb-5 md:mb-0">
+        <simple-table :header="false" :searchbox="true" title="Manage articles" border="border-gray">
+          <template v-slot:th>
+            <th class="border-t border-b border-blue-300 w-2/5 text-ideeza p-3">
+              Description
+            </th>
+            <th class="border-t border-b border-blue-300 w-1/5 text-ideeza p-3">
+              Domain
+            </th>
+            <th class="border-t border-b border-blue-300 w-1/5 text-ideeza p-3">
+              Due Date
+            </th>
+            <th class="border-t border-b border-blue-300 w-1/5 text-ideeza p-3">
+              Posted before
+            </th>
+          </template>
+          <tr class="flex w-full mb-4" v-for="(project,index) in projects">
+            <td class="w-2/5" :class="{'border-b':projects.length-1 != index}">
+              <div class="font-bold text-black">
+                {{project.description.title}}
+              </div>
+              <div class="text-sm">
+                {{project.description.text}}
+              </div>
+            </td>
+            <td class="w-1/5" :class="{'border-b':projects.length-1 != index}">{{project.domain}}</td>
+            <td class="w-1/5" :class="{'border-b':projects.length-1 != index}">{{project.due_date}}</td>
+            <td class="w-1/5" :class="{'border-b':projects.length-1 != index}">{{project.posted_before}}</td>
+          </tr>
+        </simple-table>
+      </div>
+      <div class="md:w-3/12 md:mx-3">
+        <!-- <vc-calendar color="pink" is-expanded :theme="theme" /> -->
+        <vc-calendar class="mx-auto bg-white box-shadow rounded mb-3" color="pink" is-expanded :theme="theme" />
+        <div class="bg-white shadow rounded">
+            <div class="py-3 px-5 bg-ideeza text-white text-center rounded">3rd March</div>
+            <hr class="my-1">
+            <ul class="shadow-lg">
+                 <li class="flex justify-between hover:bg-ideeza-dark py-3 px-5 event">
+                    <div>
+                        <div class="text text-sm font-bold">Finish ELectronics Part</div>
+                        <div class="text text-xs">Completed</div>
+                    </div>
+                    <div class="event-icons text-right">
+                         <font-awesome-icon class="text text-xs text-gray-500 mr-2" :icon="['fa', 'pen']" />
+                        <font-awesome-icon class="text text-sm text-gray-500" :icon="['fas', 'times']" />
+                    </div>
+                </li>
+                <li class="flex justify-between hover:bg-ideeza-dark py-3 px-5 event">
+                    <div>
+                        <div class="text text-sm font-bold">Finish ELectronics Part</div>
+                        <div class="text text-xs">Completed</div>
+                    </div>
+                    <div class="event-icons text-right">
+                         <font-awesome-icon class="text text-xs text-gray-500 mr-2" :icon="['fa', 'pen']" />
+                        <font-awesome-icon class="text text-sm text-gray-500" :icon="['fas', 'times']" />
+                    </div>
+                </li>
+                <li class="flex justify-between hover:bg-ideeza-dark py-3 px-5 event">
+                    <div>
+                        <div class="text text-sm font-bold">Finish ELectronics Part</div>
+                        <div class="text text-xs">Completed</div>
+                    </div>
+                    <div class="event-icons text-right">
+                         <font-awesome-icon class="text text-xs text-gray-500 mr-2" :icon="['fa', 'pen']" />
+                        <font-awesome-icon class="text text-sm text-gray-500" :icon="['fas', 'times']" />
+                    </div>
+                </li>
+                <li class="flex justify-between hover:bg-ideeza-dark py-3 px-5 event">
+                    <div>
+                        <div class="text text-sm font-bold">Finish ELectronics Part</div>
+                        <div class="text text-xs">Completed</div>
+                    </div>
+                    <div class="event-icons text-right">
+                         <font-awesome-icon class="text text-xs text-gray-500 mr-2" :icon="['fa', 'pen']" />
+                        <font-awesome-icon class="text text-sm text-gray-500" :icon="['fas', 'times']" />
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <!-- <div id="myCalendar" class="vanilla-calendar"></div> -->
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import SimpleTable from '~/components/reusables/Table.vue'
+  export default {
+    layout: 'user',
+    components: {
+      SimpleTable
+    },
+    mounted() {
+      new VanillaCalendar({
+        selector: "#myCalendar"
+      })
+    },
+    data() {
+      return {
+        theme: {
+          container: {
+            light: 'ideeza-date-picker',
+          },
+          arrows: {
+            light: 'ideeza-arrow',
+          },
+        },
+        blogs: [{
+            id: 1,
+            name: 'How ideeza can make the world a better place',
+            date: '10 jul, 2019',
+          },
+          {
+            id: 2,
+            name: 'How ideeza can make the world a better place',
+            date: '10 jul, 2019',
+          },
+          {
+            id: 3,
+            name: 'How ideeza can make the world a better place',
+            date: '10 jul, 2019',
+          },
+          {
+            id: 4,
+            name: 'How ideeza can make the world a better place',
+            date: '10 jul, 2019',
+          },
+        ],
+        projects: [{
+            description: {
+              title: 'Washing machine requirements',
+              text: 'lorem asdsad asd asd asdas dasd asd asd asd asdasd asd asd qweqwe qwe wqe'
+            },
+            domain: 'PCB & Asamble',
+            due_date: '01.01.2020',
+            posted_before: '10 mins'
+          },
+          {
+            description: {
+              title: 'Washing machine requirements',
+              text: 'lorem asdsad asd asd asdas dasd asd asd asd asdasd asd asd qweqwe qwe wqe'
+            },
+            domain: 'PCB & Asamble',
+            due_date: '01.01.2020',
+            posted_before: '10 mins'
+          },
+          {
+            description: {
+              title: 'Washing machine requirements',
+              text: 'lorem asdsad asd asd asdas dasd asd asd asd asdasd asd asd qweqwe qwe wqe'
+            },
+            domain: 'PCB & Asamble',
+            due_date: '01.01.2020',
+            posted_before: '10 mins'
+          },
+          {
+            description: {
+              title: 'Washing machine requirements',
+              text: 'lorem asdsad asd asd asdas dasd asd asd asd asdasd asd asd qweqwe qwe wqe'
+            },
+            domain: 'PCB & Asamble',
+            due_date: '01.01.2020',
+            posted_before: '10 mins'
+          },
+          {
+            description: {
+              title: 'Washing machine requirements',
+              text: 'lorem asdsad asd asd asdas dasd asd asd asd asdasd asd asd qweqwe qwe wqe'
+            },
+            domain: 'PCB & Asamble',
+            due_date: '01.01.2020',
+            posted_before: '10 mins'
+          }
+        ]
+      }
+    }
+  }
+
+</script>
+<style>
+.event:hover .text{
+    color: white;
+}
+</style>
