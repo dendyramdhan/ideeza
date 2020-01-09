38f3d1aa0d681246367cc41d3a74d69e2a3e4fa4
irfankhanmarwat <57019052+irfankhanmarwat@users.noreply.github.com>
2020-01-09 10:52:14 -0800

    c

--- pages/service-providers/index.vue
+++ pages/service-providers/index.vue
@@ -7,90 +7,99 @@
     <div class="flex-grow">
       <div></div>
       <nuxt-child></nuxt-child>
+
     </div>
+
   </div>
+
+
 </template>
 
 <script>
-import LeftMenu from "~/components/user/common-left-side-menu.vue";
-import SidebarMenu from "~/components/reusables/SideBar.vue";
+  import LeftMenu from '~/components/user/common-left-side-menu.vue'
+  import SidebarMenu from '~/components/reusables/SideBar.vue'
 
-export default {
-  layout: "user",
-  name: "project-index",
-  components: {
-    LeftMenu,
-    SidebarMenu
-  },
-  data: function() {
-    return {
-      menu: [
-        {
-          name: "Dashboard",
-          iconComponent: "DashBoardIcon",
-          link: "/user/dashboard"
-        },
-        {
-          name: "Users",
-          icon: "user",
-          link: "/user/home",
-          childs: [
+  export default {
+    layout: 'user',
+    name: "project-index",
+    components: {
+      LeftMenu,
+      SidebarMenu
+    },
+    data: function () {
+      return {
+        menu: [
             {
-              name: "Home",
-              link: "/user/home"
+                name: 'Dashboard',
+                iconComponent: 'DashBoardIcon',
+                link: '/user/dashboard'
             },
             {
-              name: "Contacts",
-              link: "/user/contact"
+                name: 'Users',
+                icon: 'user',
+                link: '/user/home',
+                childs: [
+                  {
+                    name: 'Home',
+                    link: '/user/home'
+                  },
+                  {
+                    name: 'Contacts',
+                    link: '/user/contact'
+                  },
+                  {
+                    name: 'Marketing',
+                    link: '/user/marketing'
+                  }
+                ]
             },
             {
-              name: "Marketing",
-              link: "/user/marketing"
+                name: 'Service Providers',
+                icon: 'users',
+                link: '/serviceprovider'
+            },
+            {
+                name: 'Investor',
+                icon: 'users',
+                link: '/investors'
+            },
+            {
+                name: 'Products',
+                icon: 'gift',
+                link: '/products'
+            },
+            {
+                name: 'Orders',
+                icon: 'shopping-cart',
+                link: '/orders'
+            },
+            {
+                name: 'Messages',
+                icon: 'envelope',
+                link: 'messages'
+            },
+            {
+              name: 'Blog',
+              icon: 'blog',
+              link: '/user/blog/updated'
             }
-          ]
-        },
-        {
-          name: "Service Providers",
-          icon: "users",
-          link: "/serviceprovider"
-        },
-        {
-          name: "Investor",
-          icon: "users",
-          link: "/investors"
-        },
-        {
-          name: "Products",
-          icon: "gift",
-          link: "/products"
-        },
-        {
-          name: "Orders",
-          icon: "shopping-cart",
-          link: "/orders"
-        },
-        {
-          name: "Messages",
-          icon: "envelope",
-          link: "messages"
-        },
-        {
-          name: "Blog",
-          icon: "blog",
-          link: "/user/blog/updated"
-        }
-      ]
-    };
-  },
-  computed: {
-    leftMenu() {
-      return this.$store.state.usermenu.openLeftMenu;
-    }
-  },
-  mounted() {},
-  methods: {}
-};
+        ]
+      }
+    },
+    computed: {
+      leftMenu() {
+        return this.$store.state.usermenu.openLeftMenu;
+      }
+    },
+    mounted() {
+
+    },
+    methods: {}
+  }
 </script>
 
 <style scoped>
+
+
+
 </style>
