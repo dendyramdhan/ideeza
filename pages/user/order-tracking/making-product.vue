b93e040412f0bbf847aba5084612edbcb3c65358
EvilCorps <11kashifali@gmail.com>
2020-01-15 21:10:45 +0500

    done

--- pages/user/order-tracking/making-product.vue
+++ pages/user/order-tracking/making-product.vue
@@ -33,8 +33,42 @@
     </div> -->
     <div>
     <h4 class="text-lg text-ideeza-black font-semibold">We have already made the cover and electronics part of your product. Now it's time to combine them</h4>
-    <div class="making-container mx-auto">
-      <div class="track-heading mt-6">Combine & QA</div>
+    <div class="making-container md:flex flex-wrap w-full md:mx-0 mx-auto">
+      <div class="flex-1 mr-10">
+      <div class="track-heading mt-6">Freelancer</div>
+      <div class="mt-10">
+        <div class="my-6" v-for="Project in Projects"  v-if="Project.kind == 'Eletronics' ">
+          <div>
+          <div class="flex justify-between items-center mb-3">
+            <div class="flex items-center">
+              <div class="mr-2">
+                <img class="avatar" :src="Project.profileimage" alt />
+              </div>
+              <div>
+                <span class="block font-semibold text-base text-black">{{Project.name}}</span>
+                <span class="block text-sm text-gray-500">{{Project.consultantKind}}</span>
+              </div>
+            </div>
+            <div class="flex items-center">
+              <nuxt-link :to="{ path: '/user/messages', query: { id: Project.id}}">
+                <font-awesome-icon
+                  class="mr-2 h-6 text-ideeza text-xl cursor-pointer"
+                  :icon="['far', 'envelope']"
+                />
+              </nuxt-link>
+            </div>
+          </div>
+          <div class="flex justify-between items-center">
+            <div class="text-gray-400 font-semibold">He's working on: <span class="text-black">Backend</span></div>
+            <a href="#" class="text-ideeza font-bold text-lg"><u>See his work</u></a>
+          </div>
+          </div>
+        </div>
+
+      </div>
+      </div>
+      <div class="flex-1 mr-10">
+      <div class="track-heading mt-6">Electronics</div>
       <div class="mt-10">
         <Progress width="60" />
         <div class="my-10" v-for="Project in Projects">
@@ -44,7 +78,7 @@
                 <img class="avatar" :src="Project.profileimage" alt />
               </div>
               <div>
-                <span class="block font-semibold text-sm">{{Project.name}}</span>
+                <span class="block font-semibold text-base text-black">{{Project.name}}</span>
                 <span class="block text-sm text-gray-500">{{Project.consultantKind}}</span>
               </div>
             </div>
@@ -63,6 +97,39 @@
           </div>
         </div>
 
+      </div>
+      </div>
+      <div class="flex-1 mr-10">
+      <div class="track-heading mt-6">Cover</div>
+      <div class="mt-10">
+        <Progress width="60" />
+        <div class="my-10" v-for="Project in Projects">
+          <div class="flex justify-between items-center" v-if="Project.kind == 'Eletronics' ">
+            <div class="flex items-center">
+              <div class="mr-2">
+                <img class="avatar" :src="Project.profileimage" alt />
+              </div>
+              <div>
+                <span class="block font-semibold text-base text-black">{{Project.name}}</span>
+                <span class="block text-sm text-gray-500">{{Project.consultantKind}}</span>
+              </div>
+            </div>
+            <div class="flex items-center">
+              <div class="text-sm text-gray-500 mr-2">
+                <span class="block font-semibold text-sm text-ideeza">{{Project.consultantname}}</span>
+                <span class="block text-xs font-bold text-gray-500">{{Project.state}}</span>
+              </div>
+              <nuxt-link :to="{ path: '/user/messages', query: { id: Project.id}}">
+                <font-awesome-icon
+                  class="mr-2 h-6 text-ideeza text-xl cursor-pointer"
+                  :icon="['far', 'envelope']"
+                />
+              </nuxt-link>
+            </div>
+          </div>
+        </div>
+
+      </div>
       </div>
     </div>
     <div class="text-right absolute right-0 bottom-0">
@@ -129,11 +196,6 @@ export default {
 </script>
 
 <style scoped>
-.making-container {
-  @apply px-2;
-  width: 100%;
-  max-width: 370px;
-}
 .track-heading {
   @apply text-2xl font-semibold relative;
   width: fit-content;
