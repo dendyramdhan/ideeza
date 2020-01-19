96919a3c401779317a51f229f0d0ec5f0c19fb55
EvilCorps <11kashifali@gmail.com>
2020-01-19 13:16:59 +0500

    starting service provider project

---
+++ components/user/pro/app/SideBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="right-bar fixed bg-white shadow">
+        <component v-bind:is="currentMenu"></component>
+    </div>
+</template>
+<script>
+import WebViewer from './WebViewer'
+import Layers from './Layers'
+import TimePicker from './TimePicker'
+export default {
+    components: {
+        WebViewer,
+        Layers,
+        TimePicker
+    },
+    data() {
+        return{
+            currentMenu: 'time-picker'
+        }
+    }
+}
+</script>
+<style scoped>
+.green-btn{
+    background:#03be33;
+  }
+</style>
