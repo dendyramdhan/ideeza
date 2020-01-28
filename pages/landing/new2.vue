5994dc61d2bc33032f70ee35726ee6fd6ad8a4bb
irfankhanmarwat <57019052+irfankhanmarwat@users.noreply.github.com>
2020-01-28 07:59:49 -0800

    new landing pages

---
+++ pages/landing/new2.vue
@@ -0,0 +1,200 @@
+<template>
+  <div class="landingBanner-bg2">
+      <top-header />
+      <nuxt />
+  	<div class="w-full pb-32">
+      <div class="container lg m-auto text-white mt-20 px-5 py-8">
+          <div class="pb-5 mb-5 px-5 md:w-6/12 w-full">
+            <p class="text-lg text-purple-900 pr-5">Together we grow the community knowledge to a new level.</p>
+            <h2 class="md:text-3xl sm:text-4xl mt-5 font-bold text-gray-700">It's more then technology, it's your home!</h2>
+            
+             
+          </div>
+           
+        </div>
+      </div>
+ 
+<div class="w-full relative py-6 mt-32">
+  <div class="container lg m-auto">
+    <div class="md:flex sm:block justify-end">
+      <img src="~/static/images/ref_img_1248.png" class="md:absolute static left-0 max-width-half">
+      <div class="md:w-6/12 sm:w-full pt-10 ">
+        <h3 class="text-purple-900 text-2xl font-bold">The power of</h3>
+        <h2 class="text-gray-900 text-5xl font-bold">TOGETHER!</h2>
+        <p class="text-gray-700 text-lg py-5 ">We believe in collaboration and together with the power of Artificial Intelligence it leads to amazing results, empowering the full potential of the world! For the first time, you can use, contribute, and empower the community knowledge - by a click, thousands of brains will come together to provide the unique solution for your idea!</p>
+        <p class="text-gray-700 text-lg py-5 ">We believe in collaboration and together with the power of Artificial Intelligence it leads to amazing results</p>
+         
+      </div>
+    </div>
+</div>
+</div>
+
+
+<div class="w-full relative mt-20 py-6">
+	<div class="container lg m-auto ">
+    <div class="md:flex sm:block">
+      <div class="md:w-6/12 sm:w-full px-3">
+        <h3 class="text-purple-900 text-2xl font-bold">Empower your ideas</h3>
+      	<h2 class="text-gray-900 text-4xl font-bold">BY YOURSELF!</h2>
+        <p class="text-gray-700 text-lg py-5 ">IDEEZA enables anyone, even without technology knowledge to take full ownership on their products development process, by using its powerful AI engine and an automated process to fulfill any dream!</p>
+        <p class="text-gray-700 text-2xl font-bold py-5 bg-i">
+        	<nuxt-link to="" class="text-white py-2 btn-md bg-ideeza inline-block text-center text-lg rounded-full">Try Free</nuxt-link>
+        </p>
+      </div>
+      <img src="~/static/images/ref_img_1249.png" class="md:absolute static right-0 max-width-half">
+    </div>
+</div>
+</div>
+
+<div class="w-full relative py-6 mt-32">
+  <div class="container lg m-auto">
+    <div class="md:flex sm:block justify-end">
+      <img src="~/static/images/ref_img_1250.png" class="md:absolute static left-0 max-width-half">
+      <div class="md:w-6/12 sm:w-full pt-10 ">
+        <h3 class="text-purple-900 text-2xl font-bold">The power of</h3>
+        <h2 class="text-gray-900 text-5xl font-bold">TOGETHER!</h2>
+        <p class="text-gray-700 text-lg py-5 ">We believe in collaboration and together with the power of Artificial Intelligence it leads to amazing results, empowering the full potential of the world! For the first time, you can use, contribute, and empower the community knowledge - by a click, thousands of brains will come together to provide the unique solution for your idea!</p>
+        <p class="text-gray-700 text-lg py-5 ">We believe in collaboration and together with the power of Artificial Intelligence it leads to amazing results</p>
+         
+      </div>
+    </div>
+</div>
+</div>
+
+ 
+
+   
+
+  <div class="container lg m-auto mt-20">
+    <div class="mt-5 w-6/12  text-left">
+      <h3 class="text-purple-900 text-2xl font-bold">Top notch public projects </h3>
+      <h2 class="text-gray-900 text-4xl font-bold">made with IDEEZA</h2>
+      <p class="text-gray-900 text-lg">Success Stories from Ideeza’s growing comunity</p>
+    </div>
+
+    <div class="md:flex sm:block mt-20">
+
+      <div class="md:w-6/12 sm:w-full px-3">
+		<img src="~static/images/silder-placeholder.png" class="inline-block">
+      
+      </div>
+      <div class="md:w-6/12 sm:w-full px-3">
+      	<h2 class="text-gray-900 text-5xl font-bold">John Doe’s Dream</h2>
+        <p class="text-gray-700 text-lg py-5 ">If you're on the fence on whether you need a big company behind you to succeed, these individuals prove that having vision and the drive is more than enough.</p>
+
+        <p class="text-gray-700 text-2xl font-bold py-5 bg-i">
+        	<nuxt-link to="" class="text-white py-2 btn-md bg-ideeza inline-block text-center text-lg rounded-full">View Story</nuxt-link>
+        </p>
+      </div>
+    </div>
+</div>
+
+<div class="w-full relative py-6 mt-32">
+  <div class="container lg m-auto">
+    <div class="md:flex sm:block justify-end">
+      <img src="~/static/images/ref_img_1252.png" class="md:absolute static left-0 max-width-half">
+      <div class="md:w-6/12 sm:w-full pt-10 ">
+        <h3 class="text-purple-900 text-2xl font-bold">The power of</h3>
+        <h2 class="text-gray-900 text-5xl font-bold">TOGETHER!</h2>
+        <p class="text-gray-700 text-lg py-5 ">We believe in collaboration and together with the power of Artificial Intelligence it leads to amazing results, empowering the full potential of the world! For the first time, you can use, contribute, and empower the community knowledge - by a click, thousands of brains will come together to provide the unique solution for your idea!</p>
+        <p class="text-gray-700 text-lg py-5 ">We believe in collaboration and together with the power of Artificial Intelligence it leads to amazing results</p>
+         
+      </div>
+    </div>
+</div>
+</div>
+
+ <div class="w-full relative mt-20 py-6">
+  <div class="container lg m-auto ">
+    <div class="md:flex sm:block">
+      <div class="md:w-6/12 sm:w-full px-3">
+        <h3 class="text-purple-900 text-2xl font-bold">Empower your ideas</h3>
+        <h2 class="text-gray-900 text-4xl font-bold">BY YOURSELF!</h2>
+        <p class="text-gray-700 text-lg py-5 ">IDEEZA enables anyone, even without technology knowledge to take full ownership on their products development process, by using its powerful AI engine and an automated process to fulfill any dream!</p>
+        <p class="text-gray-700 text-2xl font-bold py-5 bg-i">
+          <nuxt-link to="" class="text-white py-2 btn-md bg-ideeza inline-block text-center text-lg rounded-full">Try Free</nuxt-link>
+        </p>
+      </div>
+      <img src="~/static/images/ref_img_1251.png" class="md:absolute static right-0 max-width-half">
+    </div>
+</div>
+</div>  
+
+
+
+	<div class="footer-bg bg-cover clearfix mt-24">
+    <div class="curve-bg-gradient-new pt-8 clearfix">
+      <div class="container lg m-auto text-white"> 
+        <div class="mt-5 w-4/5  m-auto text-center">
+          <h2 class="text-white md:text-3xl sm:text-3xl font-bold py-5 ">Join a growing maker community</h2>
+           
+          <button class="bg-pink-CM hover:bg-pink-400 text-white font-bold py-2 px-4 w-64 rounded-full shadow-lg mt-5">Join Now</button>
+        </div>
+        
+
+      <div class="w-full md:flex flex-wrap border-t-2 pb-10 mt-10 border-pink-600">
+        <div class="flex-1 text-center md:text-left pt-5 w-full">
+          <img src="~static/images/logo-2.png" class="inline-block">
+        </div>
+        <div class="flex-1 text-center md:text-center w-full text-white pt-10">© 2016 Ideeza All rights reserved</div>
+        <div class="flex-1 text-center md:text-right w-full text-white pt-10">
+          <nuxt-link to="" class="text-white px-3">Privacy Policy</nuxt-link> |
+          <nuxt-link to="" class="text-white px-3">Term & Condition</nuxt-link>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+  </div> 
+ </template>
+<script>
+
+
+import TopHeader from '~/components/reusables/Header';
+export default {
+   components:{
+    TopHeader
+   }
+}
+</script>
+<style>
+.landing-bg-gradient{
+  background: rgb(98,18,156);
+  background: linear-gradient(86deg, rgba(242,3,210,1) 0%,rgba(98,18,156,1) 100%);
+}
+.landingBanner-bg2{
+  background:url('~static/images/ref_img_1246.png') no-repeat right top;
+  background-size:70%;
+}
+.bg-pattern{
+  background:url('~static/images/bg-pattren.png') no-repeat;
+}
+.footer-bg{
+  background:url('~static/images/footer-bg.png') no-repeat center center;
+}
+.max-width-half{
+  max-width:45%;
+  max-height:100%;
+  top:0;
+  bottom:0;
+  margin:auto;
+}
+.curve-bg-gradient-new{
+    background: rgb(98,18,156);
+  background: linear-gradient(180deg, rgba(98,18,156,0.8) 0%, rgba(242,3,210,0.8) 100%);
+}
+.video-sec{
+  background:url('~static/images/chip-bg.png') no-repeat center center;
+  background-size:100%;
+}
+ 
+.idea-sec-bg-img{
+  background:url('~static/images/ref_bg_1232.png') no-repeat center center;
+}
+@media(max-width:736px){
+  .max-width-half{
+    max-width:100%;
+  }
+}
+</style>
