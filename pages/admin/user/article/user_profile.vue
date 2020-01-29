7654afdc7098e67e965a0968a5bcecbca2e5ddcc
irfankhanmarwat <57019052+irfankhanmarwat@users.noreply.github.com>
2020-01-29 12:21:44 -0800

    added new pages

---
+++ pages/admin/user/article/user_profile.vue
@@ -0,0 +1,318 @@
+<template>
+  <div>
+    <!-- Main Contents -->
+    <div class="flex-grow mb-20">
+      <div class="main-contents">
+
+    <div class="w-11/12 m-auto">     
+      <div class="w-full bg-white shadow p-6" >
+        <div class="flex mb-5">
+          <div class="w-4/12 pr-5">
+            <img src="~/static/images/ref_img_1257.png" class=" ">
+          </div>
+          <div class="w-8/12 pt-5 text-left">
+            <div class="flex">
+              <div class="w-10/12 flex">
+                 <h2 class="text-2xl font-bold text-gray-700">James Lakers</h2>
+                 <p class="text-md text-gray-600 font-bold pt-2 pl-5"><font-awesome-icon class="text-sm" :icon="['fa', 'map-marker-alt']"/> New York, USA</p>  
+              </div>
+              <div class="w-2/12"><h3 class="text-xl font-bold text-gray-500">Report User</h3></div>
+            </div>
+            <p class="text-xl text-purple-800 font-bold">Product designer</p>
+            
+            <div class="flex my-8">
+              <div class="text-lg text-gray-500 font-bold py-3">
+               <font-awesome-icon class="text-xl" :icon="['fa', 'envelope']"/>
+                Send message
+              </div>
+              <div class="px-5">
+                <button class="text-lg text-gray-500 font-bold border py-3 px-10">
+                  <font-awesome-icon class="text-xl" :icon="['fa', 'check']"/>
+                  Conect
+                </button>
+              </div>
+            </div>
+
+            <div class=" mt-3 border-b pb-4">
+              <button class="px-3 py-3 text-lg text-gray-600 text-black text-sm inline-block">
+                <font-awesome-icon class="text-lg mr-2" :icon="['far', 'eye']"/> Timeline</button> 
+              <button class="px-3 py-3 text-lg text-gray-600 text-black text-sm inline-block">
+                <font-awesome-icon class="text-lg mr-2" :icon="['fa', 'user']"/> About</button>
+              <button class="px-3 py-3 text-lg text-gray-600 text-black text-sm inline-block"> 
+                <font-awesome-icon class="text-lg mr-2" :icon="['fa', 'square']"/> Projects</button>
+              <button class="px-3 py-3 text-lg text-gray-600 text-black text-sm inline-block"> 
+                <font-awesome-icon class="text-lg mr-2" :icon="['fa', 'star']"/>Reviews</button>
+              <button class="px-3 py-3 text-lg text-ideeza text-black text-sm inline-block"> 
+                <font-awesome-icon class="text-lg mr-2" :icon="['fa', 'newspaper']"/>Articles</button>
+
+            </div> 
+
+          </div>
+           
+        </div>
+
+          <div class="flex">
+
+            <div class="w-4/12 pr-5">
+              <h2 class="text-xl font-bold text-gray-700 border-b-2 pb-3">SOCIAL MEDIA</h2>
+              <div class="flex mt-4">
+                <div class="w-8 h-8 bg-gray-500 rounded-full text-center text-white mr-2">
+                  <font-awesome-icon class="text-lg mt-2 " :icon="['fab', 'facebook-f']"/>
+                </div>
+                <div class="w-8 h-8 bg-gray-500 rounded-full text-center text-white mr-2">
+                  <font-awesome-icon class="text-lg mt-2 " :icon="['fab', 'instagram']"/>
+                </div>
+                <div class="w-8 h-8 bg-gray-500 rounded-full text-center text-white mr-2">
+                  <font-awesome-icon class="text-lg mt-2 " :icon="['fab', 'behance']"/>
+                </div>
+                <div class="w-8 h-8 bg-gray-500 rounded-full text-center text-white mr-2">
+                  <font-awesome-icon class="text-lg mt-2 " :icon="['fab', 'dribbble']"/>
+                </div>
+                
+              </div>
+              <h2 class="text-xl font-bold text-gray-700 border-b-2 pb-3 mt-8">SKILLS</h2>
+              <div class="">
+                <div class="py-2 text-lg text-purple-800 font-bold">Product design </div>
+                <div class="py-2 text-lg text-purple-800 font-bold">Electronics </div>
+                <div class="py-2 text-lg text-purple-800 font-bold">3D Modeling </div>
+                <div class="py-2 text-lg text-purple-800 font-bold">Prototyping </div>
+                <div class="py-2 text-lg text-purple-800 font-bold">3d Max </div>
+                <div class="py-2 text-lg text-purple-800 font-bold">AutoCad </div>
+                <div class="py-2 text-lg text-purple-800 font-bold">Wood</div>
+              </div>
+            </div>
+            <div class="w-8/12">
+            <div class="">
+              <h2 class="text-2xl font-bold text-gray-700 py-4">About</h2>
+              <p class="text-md text-gray-600">PCBiX is the future of the world of PCB design.<br>We enable everyone to realize technological dreams into an ideal and high quality electric circuit ready to be manufactured in a user-friendly, Today our major competitors are developing great tools for</p>
+            </div>
+
+            <div class="flex py-5 mt-5">
+              <div class="flex-1">
+                <h3 class="text-xl text-black">Dec 1, 1997</h3>
+                <p class="text-md text-gray-600">Birthday date</p>
+              </div>
+
+              <div class="flex-1">
+                <h3 class="text-xl text-black">Hiking, Read</h3>
+                <p class="text-md text-gray-600">Hobbies</p>
+              </div>
+
+              <div class="flex-1">
+                <h3 class="text-xl text-black">Nov 17, 2018</h3>
+                <p class="text-md text-gray-600">Member Since</p>
+              </div>
+            </div>
+           
+
+          <h2 class="text-2xl text-black py-4 mt-10">Articles</h2>
+          <div class="w-full bg-white shadow p-6" >
+              <div class="">
+                <h3 class="text-xl text-black py-4 ">How Ideeza became famous in 1 week</h3>
+                <p class="text-md text-gray-700">Nov 17, 2018</p>
+                <p class="text-md text-gray-600 py-4">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content her <a href="" class="text-ideeza">See more</a></p>
+              </div>
+              <div class=" mt-3 border-b pb-4 border-b">
+                <div class="px-3 text-black text-sm inline-block">
+                  <font-awesome-icon class="text-sm mr-3" :icon="['far', 'eye']"/>221</div> 
+                <div class="px-3 text-black text-sm inline-block">
+                  <img src="~/static/images/ideeza-linke.png" class="mr-3 inline-block">67</div>
+                <div class="px-3 text-black text-sm inline-block"> 
+                  <font-awesome-icon class="text-sm mr-3" :icon="['fa', 'thumbs-up']"/>76</div>
+              </div>
+
+              <div class="py-3">
+                <img src="~/static/images/ref_img_1256.png">
+                <h3 class="text-xl text-black py-4 ">How Ideeza became famous in 1 week</h3>  
+                <p class="text-md text-gray-700">Nov 17, 2018</p>
+                <p class="text-md text-gray-600 py-4">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content her <a href="" class="text-ideeza">See more</a></p>
+              </div>
+              <div class=" mt-3 border-b pb-4 border-b">
+                <div class="px-3 text-black text-sm inline-block">
+                  <font-awesome-icon class="text-sm mr-3" :icon="['far', 'eye']"/>221</div> 
+                <div class="px-3 text-black text-sm inline-block">
+                  <img src="~/static/images/ideeza-linke.png" class="mr-3 inline-block">67</div>
+                <div class="px-3 text-black text-sm inline-block"> 
+                  <font-awesome-icon class="text-sm mr-3" :icon="['fa', 'thumbs-up']"/>76</div>
+              </div>
+
+              <div class="">
+                <h3 class="text-xl text-black py-4 ">How Ideeza became famous in 1 week</h3>
+                <p class="text-md text-gray-700">Nov 17, 2018</p>
+                <p class="text-md text-gray-600 py-4">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content her <a href="" class="text-ideeza">See more</a></p>
+              </div>
+              <div class=" mt-3 border-b pb-4 border-b">
+                <div class="px-3 text-black text-sm inline-block">
+                  <font-awesome-icon class="text-sm mr-3" :icon="['far', 'eye']"/>221</div> 
+                <div class="px-3 text-black text-sm inline-block">
+                  <img src="~/static/images/ideeza-linke.png" class="mr-3 inline-block">67</div>
+                <div class="px-3 text-black text-sm inline-block"> 
+                  <font-awesome-icon class="text-sm mr-3" :icon="['fa', 'thumbs-up']"/>76</div>
+              </div>
+
+          </div>
+        </div>
+      </div>
+    </div> 
+      </div>
+    </div>
+  </div>
+    <MyIdeeza v-click-outside="onClickOutside" v-if="showMyIdeeza" />
+  </div>
+</template>
+<script>
+  import MyIdeeza from '~/components/user/my-ideeza/new-ideeza.vue'
+  import SimpleTable from '~/components/reusables/Table.vue'
+  import CoverIcon from '~/components/partials/icons/cover-icon.vue'
+  import CodeIcon from '~/components/partials/icons/code-icon.vue'
+  import LightIcon from '~/components/partials/icons/light-icon.vue'
+
+  export default {
+    components: {
+      MyIdeeza,
+      SimpleTable,
+      CoverIcon,
+      CodeIcon,
+      LightIcon
+    },
+    data: function () {
+      return {
+        showMyIdeeza: false,
+        users: [{
+            id: 1,
+            name: 'Michael Scott',
+            email: 'marchas@gmail.com',
+            type: 1,
+            date_modified: '07/04/1927',
+            role: 1
+          },
+          {
+            id: 2,
+            name: 'Michael Scott',
+            email: 'marchas@gmail.com',
+            type: 2,
+            date_modified: '07/04/1927',
+            role: 2
+          },
+          {
+            id: 3,
+            name: 'Michael Scott',
+            email: 'marchas@gmail.com',
+            type: 1,
+            date_modified: '07/04/1927',
+            role: 2
+          },
+          {
+            id: 4,
+            name: 'Michael Scott',
+            email: 'marchas@gmail.com',
+            type: 3,
+            date_modified: '07/04/1927',
+            role: 1
+          },
+          {
+            id: 5,
+            name: 'Michael Scott',
+            email: 'marchas@gmail.com',
+            type: 3,
+            date_modified: '07/04/1927',
+            role: 1
+          },
+          {
+            id: 6,
+            name: 'Michael Scott',
+            email: 'marchas@gmail.com',
+            type: 3,
+            date_modified: '07/04/1927',
+            role: 1
+          },
+        ],
+      }
+    },
+    methods: {
+      selectall(value) {
+        if (value == true) {
+          this.users.forEach(element => {
+            element.selected = true
+          });
+        } else {
+          this.users.forEach(element => {
+            element.selected = false
+          });
+        }
+        this.$forceUpdate()
+      }
+    }
+  }
+
+</script>
+<style>
+.simple-table th {
+  font-size: 0.85rem;
+}
+.simple-table [type="checkbox"]:not(:checked) + img, .simple-table [type="checkbox"]:checked + img {
+    position: relative;
+    padding-left: 1.95em;
+    cursor: pointer;
+}
+.simple-table [type="checkbox"]:not(:checked) + img:before{
+    content: '';
+    position: absolute;
+    left: 0; top: 0;
+    width: 1.25em; height: 1.25em;
+    border: 2px solid #A5C9FF;
+    background: #FFF;
+    border-radius: 4px;
+    box-shadow: inset 0 1px 3px rgba(0,0,0,.1);
+}
+.simple-table [type="checkbox"]:checked + img:before {
+    content: '';
+    position: absolute;
+    left: 0; top: 0;
+    width: 1.25em; height: 1.25em;
+    border: 2px solid #A5C9FF;
+    background: #A5C9FF;
+    border-radius: 4px;
+    box-shadow: inset 0 1px 3px rgba(0,0,0,.1);
+}
+    /* checked mark aspect */
+.simple-table [type="checkbox"]:not(:checked) + img:after,
+    [type="checkbox"]:checked + img:after {
+    content: '\2713\0020';
+    position: absolute;
+    top: .22em;
+    left: .23em;
+    font-size: 1.0em;
+    line-height: 0.8;
+    color: #fff;
+    transition: all .2s;
+    font-family: 'Lucida Sans Unicode', 'Arial Unicode MS', Arial;
+}
+    /* checked mark aspect changes */
+.simple-table [type="checkbox"]:not(:checked) + img:after {
+    opacity: 0;
+    transform: scale(0);
+}
+.simple-table [type="checkbox"]:checked + img:after {
+    opacity: 1;
+    transform: scale(1);
+}
+    /* disabled checkbox */
+.simple-table [type="checkbox"]:disabled:not(:checked) + img:before,
+    [type="checkbox"]:disabled:checked + img:before {
+    box-shadow: none;
+    border-color: #bbb;
+    background-color: #ddd;
+}
+.simple-table [type="checkbox"]:disabled:checked + img:after {
+    color: #999;
+}
+.simple-table [type="checkbox"]:disabled + img {
+    color: #aaa;
+}
+    /* accessibility */
+.simple-table [type="checkbox"]:checked:focus + img:before,
+.simple-table [type="checkbox"]:not(:checked):focus + img:before {
+    border: 2px solid #A5C9FF;
+}
+</style>
