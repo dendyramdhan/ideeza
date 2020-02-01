364cdf54ccb123f3d29abddbc46495e4440a40fb
DESKTOP-N7IC7EF\snowman <alivesoftsnowman@outlook.com>
2020-02-02 03:48:12 +0800

    update

--- pages/technician/cover/add-component/index.vue
+++ pages/technician/cover/add-component/index.vue
@@ -15,36 +15,37 @@
   import Cover from '~/components/technician/cover/add-component.vue'
   import SearchComponent from '~/components/technician/cover/search-component-list.vue'
 
-
   export default {
+
     name: "index",
+    
     data: function () {
       return {
       }
     },
+
     components: {
       Search,
       Cover,
       SearchComponent
     },
+
     computed: {
 
     },
     methods: {
 
       updateModel(e){
-
         let transData = e.partInfo.transData
         console.log("slected poart : ", e, transData)
         let obj = {'transform':{'position':[transData.transform.position[0],transData.transform.position[1],transData.transform.position[2]],
         'rotation':[transData.transform.rotation[0], transData.transform.rotation[1], transData.transform.rotation[2]],'scale':[transData.transform.scale[0], transData.transform.scale[1], transData.transform.scale[2]],
         'color': transData.transform.color},'url': transData.url}
         this.$refs.coverComponent.addImage(obj)
-
       },
+
       updateComponent(){
         this.$refs.component_list_show.update_part_list()
-
       }
 
     }
