8120e6ffbd65b5083feee2051bdda09e01365730
Aaqib <aaqibnadeem123@gmail.com>
2020-02-12 23:35:50 +0500

    indentation fixes

--- apiService/get_param.js
+++ apiService/get_param.js
@@ -1,42 +1,42 @@
 import axios from 'axios'
-export default (sendData, ctx)=>{
-  window.$nuxt.$cookies.set("loaderFlag",true)
+export default (sendData, ctx) => {
+  window.$nuxt.$cookies.set("loaderFlag", true)
 
-    axios
-      .get(process.env.base_url + sendData.url, {
-        params:sendData.param
-      })
-      .then(response => {
-        window.$nuxt.$cookies.set("loaderFlag",false)
+  axios
+    .get(process.env.base_url + sendData.url, {
+      params: sendData.param
+    })
+    .then(response => {
+      window.$nuxt.$cookies.set("loaderFlag", false)
 
-        ctx(response)
-      })
-      .catch(() => {
-        ctx(null)
-      });
-      
-    // return axios({
-    //     method: sendData.method,
-    //     url: process.env.base_url + sendData.url,
-    //     data: sendData.data,
-    //     headers: { Authorization: `Bearer ${authToken}` }
-    //   })
-    // .then(response => {
+      ctx(response)
+    })
+    .catch(() => {
+      ctx(null)
+    });
 
-    //     ctx(response)
+  // return axios({
+  //     method: sendData.method,
+  //     url: process.env.base_url + sendData.url,
+  //     data: sendData.data,
+  //     headers: { Authorization: `Bearer ${authToken}` }
+  //   })
+  // .then(response => {
 
-    // })
-    // .catch((error) => {
-    //     ctx(null)
-    // })
+  //     ctx(response)
+
+  // })
+  // .catch((error) => {
+  //     ctx(null)
+  // })
 
 
-    // return axios.get(base_url)
-    // .then((res) => {
-    //   ctx(res)
-    // })
-    // .catch((e) => {
-    //   error({ statusCode: 404, message: 'Post not found' })
-    //   ctx(null)
-    // })
+  // return axios.get(base_url)
+  // .then((res) => {
+  //   ctx(res)
+  // })
+  // .catch((e) => {
+  //   error({ statusCode: 404, message: 'Post not found' })
+  //   ctx(null)
+  // })
 }
--- apiService/index.js
+++ apiService/index.js
@@ -2,24 +2,23 @@ import axios from 'axios'
 export default (sendData, ctx) => {
   let preload = " <img src='~/assets/images/new.gif'  style='position:absolute;top:50%;left:50%' width='20%'/>";
 
-
   console.log("call loader flag : ", true)
-  window.$nuxt.$cookies.set("loaderFlag",true)
+  window.$nuxt.$cookies.set("loaderFlag", true)
 
   return axios({
-    method: sendData.method,
-    url: process.env.base_url + sendData.url,
-    data: sendData.data
-  })
+      method: sendData.method,
+      url: process.env.base_url + sendData.url,
+      data: sendData.data
+    })
     .then(response => {
-      
-      window.$nuxt.$cookies.set("loaderFlag",false)
+
+      window.$nuxt.$cookies.set("loaderFlag", false)
       console.log("call loader flag : ", false)
 
 
       ctx(response);
       // setTimeout(function(){   }, 3000);
-     
+
 
     })
     .catch((error) => {
