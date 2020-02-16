86f4b6d488b7ef55f48bcd54269b82533e4490d6
irfankhanmarwat <57019052+irfankhanmarwat@users.noreply.github.com>
2020-02-16 00:15:13 -0800

    created new static page of blog artical preview

---
+++ pages/user/blog/article-preview.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="">
+    <div class="curve-bg-gradient clearfix">
+     
+      <headercomponent>
+      </headercomponent>
+      <nuxt />
+      
+    </div>
+
+    <div class="w-full">
+      <img src="~static/images/blog-banner.png" class="inline-block">
+    </div>
+  <div class="container lg m-auto my-20 px-4">
+      <div class="w-full md:w-3/4 m-auto">
+        <div class="text-center">
+          <h1 class="text-5xl text-gray-900 py-5">How Ideeza<br>Can make the world<br>a better place</h1>
+        </div>
+
+        <p class="py-10 text-gray-600 text-lg text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+        <p class="py-10 text-gray-600 text-lg text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+        <p class="py-10 text-gray-600 text-lg text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+        </div>
+      </div>
+  </div>
+</div>
+</div> 
+</template>
+ 
