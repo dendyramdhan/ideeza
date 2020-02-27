7d2e9a9a1ca5d828416cd1ff10b8c80487c1ad90
irfankhanmarwat <57019052+irfankhanmarwat@users.noreply.github.com>
2020-02-27 15:51:03 +0500

    new pages and modifi

---
+++ pages/user/pro/app/building.vue
@@ -0,0 +1,112 @@
+<template>
+<div class="">
+	<div class="flex mt-10 border-gray-300 border-b pb-10 justify-end">
+      <div class="px-3">
+          <font-awesome-icon class="" :icon="['fas', 'bolt']"></font-awesome-icon>
+        <span class="ml-3">Electronics</span>
+      </div>
+      <div class="px-3">
+          <font-awesome-icon class="" :icon="['fas', 'code']"></font-awesome-icon>
+          <span class="ml-3">Code</span>
+      </div>
+      <div class="px-3 ">
+          <img src="~static/images/cover.png" class="inline-block mx-3">
+          <span class="ml-3 text-ideeza">Cover</span>
+      </div>
+      <div class="px-3 ">
+          <img src="~static/images/settings.png" class="inline-block mx-3">
+          <span class="ml-3">Genral</span>
+      </div>
+    </div>
+    <div class="w-full relative">
+		<img src="~/static/images/img_ref_23337.png" class="w-full">
+		<div class="absolute left-0 bottom-0 text-xl text-white pl-3 pb-3">
+			30<span class="text-sm"> mm</span> x 60<span class="text-sm">mm</span> x 70<span class="text-sm">mm</span>
+		</div>
+		<div class="absolute right-0 bottom-0 text-xl text-white pr-5 pb-3">
+			<button>
+				<font-awesome-icon class=" text-white mt-0 ml-4 text-xl" :icon="['fas', 'question']"/>
+			</button>
+			<button>
+				<font-awesome-icon class=" text-white mt-0 ml-4 text-xl" :icon="['fas', 'cog']"/>
+			</button>
+			<button>
+				<font-awesome-icon class=" text-white mt-0 ml-4 text-xl" :icon="['fas', 'layer-group']"/>
+			</button>
+			<button>
+				<font-awesome-icon class=" text-white mt-0 ml-4 text-xl" :icon="['fas', 'edit']"/>
+			</button>
+			<button>
+				<font-awesome-icon class=" text-white mt-0 ml-4 text-xl" :icon="['fas', 'edit']"/>
+			</button>
+			<button class="transform rotate-45">
+				<font-awesome-icon class=" text-white mt-0 ml-4 text-xl" :icon="['fas', 'arrows-alt-v']"/>
+			</button>
+		</div>
+	</div>
+
+	<div class="flex justify-between py-3">
+		<button class="bg-ideeza py-2 px-6 text-white rounded font-bold">Share invention <font-awesome-icon class=" text-white mt-0 ml-4 text-xl" :icon="['fas', 'star']"/></button>
+		<div class="flex">
+			<button class="text-ideeza py-2 px-6 bg-white botder border-gray-600 rounded font-bold"> 
+			<font-awesome-icon class=" text-ideeza mt-0 ml-4 text-xl" :icon="['fas', 'shopping-cart']"/>Buy now</button>
+		<span class="py-3 mr-3"><font-awesome-icon class=" text-gray-400 mr-3 mt-0 ml-4 text-xl" :icon="['fas', 'eye']"/>2.5 k</span>
+		<span class="px-3 py-2 border font-bold border-gray-600 text-gray-600">
+			<font-awesome-icon class=" text-gray-600 mt-0text-xl" :icon="['fas', 'star']"/>
+			39
+		</span>
+		</div>	
+
+	</div>
+
+	<div >
+		<div class="mb-4">
+        <label class="text-gray-800 font-bold pb-3 block">Description:</label>
+        <textarea class="shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="" type="text" placeholder="Type the description here..." rows="10"></textarea>
+      </div>
+      <div class="text-right">
+      	<button class="bg-ideeza py-2 px-6 text-white rounded font-bold">Save</button>
+      </div>
+	</div>
+</div>
+</template>
+<script>
+import Electronics from '~/components/user/pro/pro-electronics.vue'
+import Cover from '~/components/user/pro/pro-cover.vue'
+export default {
+    components: {
+        Electronics,
+        Cover
+    }
+}
+</script>
+<style scoped>
+.cover{
+    margin-top: 197px;
+}
+.sub_tool_list .sub_tool_lavel2 {
+    left: 0;
+    top: 100%;
+    display:none;
+}
+.sub_tool_lavel3 {
+	left: 0;
+    top: 100%;
+    display:none;
+}
+.bg-transp{
+ background: rgba(255,255,255,0.6);
+}
+.arrow-tip{
+	position: absolute;
+    top: -20px;
+    font-size: 21px;
+    color: #ffffffab;
+}
+.have_sublist:hover .sub_tool_lavel2{
+	display:block;
+}
+.sub_tool_lavel2:hover .sub_tool_lavel3{
+	display:block;
+}
+</style>
---
+++ pages/user/settings/personal_data.vue
@@ -0,0 +1,169 @@
+<template>
+   <div class="p-4">
+     <h2 class="text-4xl font-bold text-gray-800 mt-5">See and manage the data in your IDEEZA's account</h2>
+     <p class="py-3 text-gray-700">Your data includes the things you do, like searches, and the things you create, like new products.</p>
+
+     <div class="flex mt-5">
+       <div class="w-2/12">
+         <img src="https://randomuser.me/api/portraits/men/17.jpg">
+       </div>
+       <div class="pl-5 w-3/12">
+         <h2 class="text-2xl font-bold text-gray-800">Thomas Hellies</h2>
+         <p class="py-3 text-gray-700">thomashellies@gmail.com</p>
+         <p class="py-3 text-gray-700">Last seen 20mins ago</p>
+         <div class="flex">
+           <div class="w-10 h-10 text-center rounded-full bg-gray-700 mr-3">
+             <font-awesome-icon class="text-white text-4xl mt-2" :icon="['fab', 'facebook-f']"></font-awesome-icon>
+           </div>
+           <div class="w-12 h-12">
+             <font-awesome-icon class="text-gray-700 text-4xl mt-2" :icon="['fab', 'linkedin-in']"></font-awesome-icon>
+           </div>
+         </div>
+       </div>
+       <div class=" pl-5 w-7/12">
+         <div class="bg-white rounded shadow p-4">
+           <h2 class="text-2xl font-bold text-gray-800">About me</h2>
+         <p class="py-3 text-gray-700">Hi, I'm Thomas. I live in New York and my main job here is to help people in development software. I have 2 years of experience in this field, so in my opinion, I have the ability to help anyone in any situation when it comes to development</p>
+         </div>
+       </div>
+     </div>
+
+     <div class="flex mt-10">
+      <div class="flex-1">
+        <table>
+          <tr>
+            <td class="border-b py-2 px-2">Gender</td>
+            <td class="border-b py-2 px-2">Male</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Birth date</td>
+            <td class="border-b py-2 px-2">26th December, 2002</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Address</td>
+            <td class="border-b py-2 px-2">Vazha-Pshavela avenue, block VII, 21</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Phone number</td>
+            <td class="border-b py-2 px-2">+995 555 512 321</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Timezone</td>
+            <td class="border-b py-2 px-2">GMT +4</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Languages</td>
+            <td class="border-b py-2 px-2">English (UK) (preffered), Hebrew</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Preferred language</td>
+            <td class="border-b py-2 px-2">English (UK)</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Website</td>
+            <td class="border-b py-2 px-2">Something.com</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Skills</td>
+            <td class="border-b py-2 px-2">Hiking, Web development, House cleaning</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">School</td>
+            <td class="border-b py-2 px-2">Main school #21</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Work</td>
+            <td class="border-b py-2 px-2">Azry corp. - Senior developer</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Credit/Debit card</td>
+            <td class="border-b py-2 px-2"><img src="~/static/images/cdbd.png" class="inline-block mr-3"> **** **** **** **** 2321</td>
+          </tr>
+          
+        </table>
+      </div>
+      <div class="flex-1 pl-3">
+        <table>
+          <tr>
+            <td class="border-b py-2 px-2">Paypal account</td>
+            <td class="border-b py-2 px-2">thomashellies@gmail.com</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Ideeza account number</td>
+            <td class="border-b py-2 px-2">#381203841</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Log in history</td>
+            <td class="border-b py-2 px-2 text-ideeza">Show</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Reviews</td>
+            <td class="border-b py-2 px-2 text-ideeza">Show</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Rating</td>
+            <td class="border-b py-2 px-2">4.8/5</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Friends</td>
+            <td class="border-b py-2 px-2 text-ideeza">Show</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Messages</td>
+            <td class="border-b py-2 px-2 text-ideeza">Show shared files</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Blog articles</td>
+            <td class="border-b py-2 px-2 text-ideeza">Show</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Product/project blueprint</td>
+            <td class="border-b py-2 px-2 text-ideeza">Show</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Tasklist</td>
+            <td class="border-b py-2 px-2 text-ideeza">Show</td>
+          </tr>
+          <tr>
+            <td class="border-b py-2 px-2">Track order</td>
+            <td class="border-b py-2 px-2 text-ideeza">2 active products</td>
+          </tr>
+           
+        </table>
+      </div>
+     </div>
+   </div>
+</template>
+
+<script>
+import Projects from "~/data/TechnicianSettingApi.json";
+import apiService from "~/apiService/have_token.js";
+
+export default { 
+};
+</script>
+
+<style scoped>
+  .settings-general{
+    max-width: 700px;
+  }
+  .form-field{
+    @apply py-4 border-b border-solid border-gray-300;
+  }
+  .form-field:last-child{
+    @apply border-0;
+  }
+  .field-label{
+    @apply w-56 text-gray-600 text-xl font-semibold;
+  }
+
+  @screen md{
+    .form-field{
+      @apply flex;
+    }
+  }
+  select.w-full{
+    width: 100% !important;
+    max-width: 100% !important;
+  }
+</style>
